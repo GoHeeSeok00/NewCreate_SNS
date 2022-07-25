a361d0dfd9db223835dc9c64d2e3c320b85d0b46
Github <weeds1590@gmail.com>
2022-07-25 11:49:24 +0900

    Add : feature/#4 게시글 좋아요 모델 추가
    - 사용자는 한 포스터에 한번만 좋아요를 할 수 있어야 되기에 UniqueConstraint 설정
    - db에 모델 변경사항 반영

---
+++ post/models/post_like.py
@@ -0,0 +1,33 @@
+from django.db import models
+
+from post.models.post import Post as PostModel
+from user.models import User as UserModel
+
+
+class PostLike(models.Model):
+    """
+    Assignee : 고희석
+    Date : 2022.07.25
+
+    게시글 좋아요를 위한 모델입니다.
+
+    unique constraints 설정으로 한 사용자가 한 게시글에 좋아요를 한번만 할 수 있습니다.
+    """
+
+    user = models.ForeignKey(
+        to=UserModel,
+        verbose_name="사용자",
+        on_delete=models.CASCADE,
+        related_name="post_like",
+    )
+    post = models.ForeignKey(
+        to=PostModel,
+        verbose_name="게시글",
+        on_delete=models.CASCADE,
+        related_name="post_like",
+    )
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=["user", "post"], name="unique_user_post"),
+        ]
